fe9bd6214b20db07c0ced54377d61b0decb56a3f
Anastasia <asiaa2110@gmail.com>
2023-01-22 14:52:43 +0300

    Initial commit

---
+++ src/main/java/ru/netology/stats/services/StatsService.java
@@ -0,0 +1,71 @@
+package ru.netology.stats.services;
+
+public class StatsService {
+
+   public int calcSumSales (int[] sales) {
+       int sum = 0;
+       for (int i: sales) {
+           sum += i;
+       }
+       return sum;
+   }
+
+    public int calcMiddleSumSales (int[] sales) {
+        int middleSum = calcSumSales(sales) / sales.length;
+        return middleSum;
+    }
+
+    public int minSales(long[] sales) {
+        int minMonth = 0;
+        int month = 0; // переменная для индекса рассматриваемого месяца в массиве
+        for (long sale : sales) {
+            // sales[minMonth] - продажи в месяце minMonth
+            // sale - продажи в рассматриваемом месяце
+            if (sale <= sales[minMonth]) {
+                minMonth = month;
+            }
+            month = month + 1; // следующий рассматриваемый месяц имеет номер на 1 больше
+        }
+        return minMonth + 1;
+    }
+
+    public int maxSales(long[] sales) {
+        int maxMonth = 0;
+        int month = 0;
+        for (long sale : sales) {
+            if (sale >= sales[maxMonth]) {
+                maxMonth = month;
+            }
+            month = month + 1; // следующий рассматриваемый месяц имеет номер на 1 больше
+        }
+        return maxMonth + 1;
+    }
+
+    public int calcNumberMonthsWithSalesLowerMiddle(int[] sales) {
+        int numberMonths = 0;
+        int sum =0;
+        for (int monthSale : sales) {
+            sum += monthSale;
+        }
+        for (int sale : sales) {
+            if (sale < calcMiddleSumSales(sales)) {
+                numberMonths += 1;
+            }
+        }
+        return numberMonths;
+    }
+
+    public int calcNumberMonthsWithSalesHigherMiddle(int[] sales) {
+        int numberMonths = 0;
+        int sum =0;
+        for (int monthSale : sales) {
+            sum += monthSale;
+        }
+        for (int sale : sales) {
+            if (sale > calcMiddleSumSales(sales)) {
+                numberMonths += 1;
+            }
+        }
+        return numberMonths;
+    }
+}
---
+++ src/test/java/ru/netology/stats/services/StatsServiceTest.java
@@ -0,0 +1,78 @@
+package ru.netology.stats.services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class StatsServiceTest {
+
+    @Test
+    public void shouldCalcSumSales() {
+        StatsService service = new StatsService();
+
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        int expectedSum = 180;
+        int actualSum = service.calcSumSales(sales);
+
+        Assertions.assertEquals(expectedSum, actualSum);
+    }
+
+    @Test
+    public void shouldCalcMiddleSumSales() {
+        StatsService service = new StatsService();
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        int expectedMiddleSum = 15;
+        int actualMiddleSum = service.calcMiddleSumSales(sales);
+
+        Assertions.assertEquals(expectedMiddleSum, actualMiddleSum);
+    }
+
+    @Test
+    public void shouldFindTheNumberOfTheMonthOfMinSales() {
+        StatsService service = new StatsService();
+
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long expectedMonth = 9;
+        long actualMonth = service.minSales(sales);
+
+        Assertions.assertEquals(expectedMonth, actualMonth);
+    }
+
+    @Test
+    public void shouldFindTheNumberOfTheMonthOfMaxSales() {
+        StatsService service = new StatsService();
+
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long expectedMonth = 8;
+        long actualMonth = service.maxSales(sales);
+
+        Assertions.assertEquals(expectedMonth, actualMonth);
+    }
+
+    @Test
+    public void shouldCalcNumberMonthsWithSalesLowerMiddle() {
+        StatsService service = new StatsService();
+
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        int expectedNumberMonths = 5;
+        int actualNumberMonths = service.calcNumberMonthsWithSalesLowerMiddle(sales);
+
+        Assertions.assertEquals(expectedNumberMonths, actualNumberMonths);
+    }
+
+    @Test
+    public void shouldCalcNumberMonthsWithSalesHigherMiddle() {
+        StatsService service = new StatsService();
+
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        int expectedNumberMonths = 5;
+        int actualNumberMonths = service.calcNumberMonthsWithSalesHigherMiddle(sales);
+
+        Assertions.assertEquals(expectedNumberMonths, actualNumberMonths);
+    }
+}
